c82c67dc95db324e2aac3d7f258a8df4e0129c35
unknown <carv3l@vivaldi.net>
2020-06-06 23:56:07 +0100

    added connection to atlas and env file

--- Backend/server.js
+++ Backend/server.js
@@ -8,9 +8,21 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 8080;
 
+const uri = process.env.ATLAS_URI;
+
 app.use(cors());
 
 app.use(express.json());
+
+
+mongoose.connect(uri, { useNewUrlParser: true});
+var db = mongoose.connection;
+
+if(!db)
+    console.log("Error connecting db")
+else
+    console.log("Db connected successfully")
+
 
 app.listen(port, function () {
     console.log("Running Server on port: " + port);
