2ab937410c9e93495b1fced76ca5fe640c9b6495
unknown <carv3l@vivaldi.net>
2020-06-07 13:49:29 +0100

    added function add in routes

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-let eletrodos = require('../models/eletrodo.model');
+let Eletrodo = require('../models/eletrodo.model');
 
 router.route('/').get((req, res) => {
         eletrodos.get(function (err, eletrodo) {
@@ -17,3 +17,21 @@ router.route('/').get((req, res) => {
         });
 
     });
+
+    router.route('/add').post((req, res) => {
+        var dbeletrodo = new Eletrodo();
+
+        dbeletrodo.idsolo = req.body.idsolo;
+        dbeletrodo.solo = req.body.solo;
+        dbeletrodo.resistividade = req.body.resistividade;
+        dbeletrodo.coordenadas = req.body.coordenadas;
+      
+        
+        dbeletrodo.save()
+        .then(() => res.json({message: 'Novo Eletrodo Adicionado!',data: dbeletrodo}))
+        .catch(err => res.status(400).json('Erro'+ err));    
+        console.log(dbeletrodo);    
+        
+    });
+
+    module.exports = router;
