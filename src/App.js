64fe6d0ec175064603d94beefec72c9e3fe4495c
unknown <carv3l@vivaldi.net>
2020-06-08 20:20:08 +0100

    addsoil component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import Navbar from "./components/navbar.component"
 import ExercisesList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercise.component";
-import CreateExercise from "./components/create-exercise.component";
+import CreateExercise from "./components/addsoil.component";
 import CreateUser from "./components/create-user.component";
 
 function App() {
---
+++ src/components/addsoil.component.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
+export default class CreateExercise extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeIdSolo = this.onChangeIdSolo.bind(this);
+    this.onChangeSolo = this.onChangeSolo.bind(this);
+    this.onChangeResistividade = this.onChangeResistividade.bind(this);
+    this.onChangeCoordenadas= this.onChangeCoordenadas.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      idsolo: '',
+      solo: '',
+      resistividade: '',
+      coordenadas: '',
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/users/')
+      .then(response => {
+        if (response.data.length > 0) {
+          this.setState({
+            users: response.data.map(user => user.username),
+            username: response.data[0].username
+          })
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
+  }
+
+  onChangeIdSolo(e) {
+    this.setState({
+      idsolo: e.target.value
+    })
+  }
+
+  onChangeSolo(e) {
+    this.setState({
+      solo: e.target.value
+    })
+  }
+
+  onChangeResistividade(e) {
+    this.setState({
+      onResistividade: e.target.value
+    })
+  }
+
+  onChangeCoordenadas(e) {
+    this.setState({
+      coordenadas : e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const datasoil = {
+      idsolo: this.state.idsolo,
+      solo: this.state.solo,
+      resistividade: this.state.resistividade,
+      coordenadas: this.state.coordenadas
+    }
+
+    console.log(exercise);
+
+    axios.post('http://localhost:5000/exercises/add', datasoil)
+      .then(res => console.log(res.data));
+
+    window.location = '/';
+  }
+
+  render() {
+    return (
+    <div>
+      <h3>Add Soil</h3>
+      <form onSubmit={this.onSubmit}>
+        <div className="form-group"> 
+          <label>Id Solo: </label>
+          <select ref="userInput"
+              required
+              className="form-control"
+              value={this.state.idsolo}
+              onChange={this.onChangeIdSolo}>
+             
+          </select>
+        </div>
+        <div className="form-group"> 
+          <label>Solo: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={this.state.solo}
+              onChange={this.onChangeSolo}
+              />
+        </div>
+        <div className="form-group">
+          <label>Resistividade: </label>
+          <input 
+              type="text" 
+              className="form-control"
+              value={this.state.resistividade}
+              onChange={this.onChangeResistividade}
+              />
+        </div>
+        <div className="form-group">
+          <label>Coordenadas: </label>
+          <input 
+              type="text" 
+              className="form-control"
+              value={this.state.coordenadas}
+              onChange={this.onChangeCoordenadas}
+              />
+        </div>
+
+        <div className="form-group">
+          <input type="submit" value="Add Soil" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
+    )
+  }
+}
