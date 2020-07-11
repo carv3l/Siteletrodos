1ec5ff9d936c82d29898b2224311817e6985f0ae
carv3l <1012208@sal.ipg.pt>
2020-07-11 12:39:03 +0100

    first iteration of the graph

--- src/components/graph.component.js
+++ src/components/graph.component.js
@@ -1,44 +1,48 @@
 import React, { Component } from 'react';
-import Plotta from 'dist/plotta.js';
+import Plotta from 'plotta.js';
 
 
-const canvas = document.getElementById("canvas");
-const plotta = new Plotta(canvas, {
-    linedatas: [
-      {
-        id: "line1",
-        type: "func",
-        legend: "cos",
-        color: "#55A8DE",
-        visible: true,
-        func: Math.cos,
-        dotNum: 1000
-      }
-    ],
-    config: {
-      title: {
-        location: "center",
-        color: "#666666",
-        text: "Plotta.js"
-      },
-      grid: {
-        visible: true,
-        color: "#888888"
-      },
-      border: {
-        visible: true,
-        color: "#DDDDDD",
-        width: 1
-      }
-    }
-  })
+
 export default class graph extends Component {
     constructor(props) {
-        super(props);
-    
+        super(props); 
         
        }
-    
+       componentDidMount() {
+        const canvas = document.getElementById("canvas");
+        const plotta = new Plotta(canvas, {
+            linedatas: [
+              {
+                id: "line1",
+                type: "func",
+                legend: "cos",
+                color: "#55A8DE",
+                visible: true,
+                func: Math.cos,
+                dotNum: 1000
+              }
+            ],
+            config: {
+              title: {
+                location: "center",
+                color: "#666666",
+                text: "Medidas"
+              },
+              
+              grid: {
+                visible: true,
+                color: "#888888"
+              },
+              border: {
+                visible: true,
+                color: "#DDDDDD",
+                width: 200
+              }
+            }
+          })
+
+
+       }
         
     
 render() {
@@ -47,7 +51,7 @@ render() {
   id="canvas"
   width="700px"
   height="700px"
-  style="width:700px; height:700px; border:1px solid #d3d3d3;"
+  style={{width: '700px', height:'700px', border:'1px'}}
 ></canvas>
     )}
 }
