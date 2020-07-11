99b54076566169fb20e37b5d3f65a53d346038a5
carv3l <1012208@sal.ipg.pt>
2020-07-11 13:33:03 +0100

    added chart component as plotta not useful

---
+++ src/components/chart.component.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+
+export default class chart extends Component {
+    constructor(props) {
+        super(props); 
+        
+       }
+       componentDidMount() {
+  
+         
+       }
+        
+    
+render() {
+  
+    return (
+<canvas
+  id="canvas"
+  width="700px"
+  height="700px"
+  style={{width: '700px', height:'700px', border:'1px'}}
+></canvas>
+    )}
+}
