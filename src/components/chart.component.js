9df60cead7bd7ef6ce5387668d053a202b31fbb0
carv3l <1012208@sal.ipg.pt>
2020-07-12 11:22:45 +0100

    Implemented average serie on chart, and added labels

--- src/components/chart.component.js
+++ src/components/chart.component.js
@@ -23,21 +23,25 @@ constructor(props) {
           categories: []
         },
         markers: {
-          size: 5,
+          size: [5, 0],//Tamanho do ponto, 5 para a primeira serie, 0 para a média
       },    
       stroke: {
         curve: 'smooth',
       }
       },
-      legend: {
-        show: true,
-        position: 'bottom'
-      },  
+      dataLabels: {
+        enabled: true
+      },
+      colors: ["#FF1654", "#247BA0"],
       series: [
         {
           name: "series-1",
           data: []
-        }
+        },
+        {
+          name: "series-2",
+          data: []
+        },
     ]
     };
   }
@@ -66,11 +70,20 @@ constructor(props) {
 
         //Criacao de um array com valores da bd para o Y
         var array_rsolo =[];
+        var soma=0;
+        var media=0;
         for (var i = 0; i < medida.length; i++) {
           array_rsolo.push(medida[i]['r_solo']);
-
+          soma+=medida[i]['r_solo'];//dentro do loop tambem faz a soma para a média
         }
-        
+        media= Math.round((soma/medida.length) * 100) / 100;
+
+        //Colocar a media num array para mostrar no grafico
+        //alert("media"+media);
+        var media_array=[];
+        for (let index = 0; index < medida.length; index++) {
+          media_array.push(media);
+        }        
         //Criacao de um array com valores da bd para o x (numero medidas)
         var array_nmedidas =[];
         for (var i = 0; i < medida.length; i++) {
@@ -80,18 +93,20 @@ constructor(props) {
 
 
         this.setState(prevState => {
-            let options = Object.assign({}, prevState.options); // creating copy of state variable jasper
+            let options = Object.assign({}, prevState.options); // creating copy of state variable options
             options.xaxis.categories = array_nmedidas;                     // update the name property, assign a new value                 
-            return { options };                                 // return new object jasper object
+            return { options };                                 // return new object options object
           })
         //alert("Oi"+this.state.options.xaxis.categories);
 
         console.log(array_rsolo);
-        var name = "Medida 1";
+        var name1 = "Medida";
+        var name2 = "Average";
         this.setState(prevState => {                        
-            let series = { ...prevState.series[0] };            // creating copy of state variable serie 
-            series = JSON.stringify(series);                    //convertin object to string
-            series = JSON.parse('[{"name":"'+name+'","data":['+array_rsolo+']}]'); // update and parsing to object                                  
+            let series = { ...prevState.series };           // creating copy of state variable serie 
+          //  alert("in"+JSON.stringify(series));
+            series = JSON.stringify(series); //convertin object to string
+            series = JSON.parse('[{"name":"'+name1+'","data":['+array_rsolo+']},{"name":"'+name2+'","data":['+media_array+']}]'); // update and parsing to object                                  
             return {series};                                 // return new object
           }); 
 
@@ -114,7 +129,7 @@ constructor(props) {
               options={this.state.options}
               series={this.state.series}
               type="line"
-              width="1160"
+              width="860"
             />
           </div>
         </div>
