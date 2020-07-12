84a503fb9fac9a4b704bec317fbb6fad3e8a25d5
unknown <carv3l@vivaldi.net>
2020-07-12 22:16:29 +0100

    fixed bug where was replicating data

--- src/components/chart.component.js
+++ src/components/chart.component.js
@@ -10,50 +10,53 @@ var uri_get ="http://localhost:8080/measures/";
 var array_media= [];
 var array_nmedidas = [];
 var array_rsolo = [];
+var medida;
 var vtoggle = false;
+const initialState = {
+  options: {
+    chart: {
+      id: "basic-line",
+      },
+    tooltip: {
+      enabled: true,
+      },
+    xaxis: {
+      categories: []
+    },
+    markers: {
+      size: [5, 0],//Tamanho do ponto, 5 para a primeira serie, 0 para a média
+  },    
+  stroke: {
+    curve: 'smooth',
+    //width: [1, 4]
+  },
+  dataLabels: {//mostra o valor nos pontos
+    enabled: vtoggle,
+    enabledOnSeries: [0, 1]
+  }
+  },
+  labels: ['01 Jan 2001', '02 Jan 2001'],
+  colors: ["#FF1654", "#247BA0"],
+  series: [
+    {
+      name: "series-1",
+      data: []
+    },
+    {
+      name: "series-2",
+      data: []
+    },
+]
+};
+
+
 class App extends Component {
+
+// shouldComponentUpdate = () => false;
 constructor(props) {
     super(props);  
   this.state = {measure: []};
-    this.state = {
-      options: {
-        chart: {
-          id: "basic-line",
-          },
-        tooltip: {
-          enabled: true,
-          },
-        xaxis: {
-          categories: []
-        },
-        markers: {
-          size: [5, 0],//Tamanho do ponto, 5 para a primeira serie, 0 para a média
-      },    
-      stroke: {
-        curve: 'smooth',
-        //width: [1, 4]
-      },
-      dataLabels: {//mostra o valor nos pontos
-        enabled: vtoggle,
-        enabledOnSeries: [0, 1]
-      }
-      },
-      labels: ['01 Jan 2001', '02 Jan 2001'],
-      colors: ["#FF1654", "#247BA0"],
-      series: [
-        {
-          name: "series-1",
-          data: []
-        },
-        {
-          name: "series-2",
-          data: []
-        },
-    ]
-    };
-
-    
-    
+  this.state = initialState;
   }
 //async e await para esperar que haja response do axios
   async componentDidMount() {
@@ -70,13 +73,12 @@ constructor(props) {
         })
       
         //alert(this.state.options.xaxis.categories)
-        var medida = this.state.measure;
+        medida = this.state.measure;
     
         console.log(medida.length);
-        
-        this.setState({categories : medida.length});
-        
+       
         console.log(medida);
+      
 
         //Criacao de um array com valores da bd para o Y
        
@@ -119,14 +121,20 @@ constructor(props) {
             series = JSON.parse('[{"name":"'+name1+'","data":['+array_rsolo+']},{"name":"'+name2+'","data":['+array_media+']}]'); // update and parsing to object                                  
             return {series};                                 // return new object
           }); 
-
-       // this.setState({categories: array_rsolo});
-       
+          this.reset();
   }
+  //shouldComponentUpdate(){
+   // this.setState(initialState)
+ // }
 
+
+  //Fazer reset ao compoene
+  //componentWillUnmount() { 
+  //  this.setState(initialState)
+  //}
 
   async onSubmit(e) {
-    
+    //this.setState(initialState)
  
     vtoggle=true;
 
@@ -138,9 +146,6 @@ constructor(props) {
     this.setState({ options: this.state.options})
 
   }
-
-
-
   stratifiedSoil(e) {
     var count = 0;
     for (let i= 0; i < array_nmedidas.length; i++) {
@@ -160,16 +165,19 @@ constructor(props) {
       alert("SOLO NÃO HOMOGENEO \n Nº  Medidas Compativeis: "+count);
     }
 
-    this.forceUpdate();
   }
 
+  reset(){
+    array_media= [];
+    array_nmedidas = [];
+    array_rsolo = [];
+  }
 
 
   render() {
     //console.log("ola categories "+ JSON.stringify(this.state.options));
    // alert("Oi"+this.state.options.dataLabels.enabled);
    console.log(this.state.options);
-
     return (
       <div className="app">
         <div className="row">
