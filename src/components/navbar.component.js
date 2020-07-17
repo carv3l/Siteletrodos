af6d07db2d6a953c8a3bb5da009d30dc07807e58
unknown <carv3l@vivaldi.net>
2020-07-17 17:52:36 +0100

    navbar user session implementation

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+
 export default class Navbar extends Component {
 
   render() {
@@ -13,7 +14,7 @@ export default class Navbar extends Component {
           <li className="navbar-item">
           <Link to="/" className="nav-link">Calcular</Link>
           </li>
-          {/* <li className="navbar-item">
+         {/*  <li className="navbar-item">
           <Link to="/AddSoil" className="nav-link">Add Solo</Link>
           </li>
           <li className="navbar-item">
@@ -28,14 +29,16 @@ export default class Navbar extends Component {
           <li className="navbar-item">
           <Link to="/Login" className="nav-link">Login</Link>
           </li>
-          <li className="navbar-item">
-          <label id="session_user"></label>
-          </li>
-
         </ul>
+        <li className="navbar-item">
+        <label id="session_user">OI</label>
+        </li>
+        
         </div>
         </div>
-      </nav>
+      </nav> 
     );
   }
 }
+
+
